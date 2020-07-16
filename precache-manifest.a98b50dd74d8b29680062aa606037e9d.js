e412d7dd2136eb1bf5f94ce1fcd2b8d9c3a6523e
WiktorKS <wiktorks1994@gmail.com>
2020-07-16 20:01:22 +0200

    Updates

---
+++ precache-manifest.a98b50dd74d8b29680062aa606037e9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc12cffd1c8aedb784c3d8ac50585f6",
+    "url": "/adapter/index.html"
+  },
+  {
+    "revision": "99c5dc694e2f3e254235",
+    "url": "/adapter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0eff3b033eef99c154f4",
+    "url": "/adapter/static/js/2.749877a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adapter/static/js/2.749877a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c5dc694e2f3e254235",
+    "url": "/adapter/static/js/main.8c9306e7.chunk.js"
+  },
+  {
+    "revision": "58dc2b85fe26d1518590",
+    "url": "/adapter/static/js/runtime-main.9be9f57f.js"
+  }
+]);
